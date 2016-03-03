18d799daf914de39b53e1238167237a3fd9e6ca2
jvoigtlaender <jvoigtlaender@users.noreply.github.com>
2016-03-03 13:58:04 +0100

    Travis-based deployment of examples

---
+++ deploy-examples.sh
@@ -0,0 +1,24 @@
+rm -rf out || exit 0;
+
+npm install -g elm
+
+elm-package install --yes
+elm-package install evancz/elm-html --yes
+elm-package install evancz/elm-effects --yes
+elm-package install evancz/start-app --yes
+elm-make --yes
+
+mkdir out
+elm-make Example1.elm --output out/Example1.html --yes
+elm-make Example2.elm --output out/Example2.html --yes
+elm-make Example3.elm --output out/Example3.html --yes
+elm-make Example3b.elm --output out/Example3b.html --yes
+elm-make Example4.elm --output out/Example4.html --yes
+cd out
+
+git init
+git config user.name "Travis CI"
+git config user.email "jvoigtlaender@users.noreply.github.com"
+git add .
+git commit -m "Travis deploy examples to gh-pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages >/dev/null 2>&1
